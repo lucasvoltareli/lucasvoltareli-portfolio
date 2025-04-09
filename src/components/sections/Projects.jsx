e76418b6219f77e2a6507c584fdcddb62cd923ce
lucasvoltareli <pietro9919@outlook.com>
2025-04-09 02:33:58 -0300

    Corrigido conflito no package.json

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,14 +1,8 @@
 import {RevealOnScroll} from '../RevealOnScroll'
-<<<<<<< HEAD
 import notBullSht from '../../assets/projects/notbullsht.png'
 import papaleg from '../../assets/projects/papaleg.png'
 import leaff from '../../assets/projects/first-ecommerce.png'
 import devlinks from '../../assets/projects/devlinks.png'
-
-=======
-import notBullSht from '../../assets/notbullsht.png'
-import papaleg from '../../assets/papaleg.png'
->>>>>>> 16f34e0a4ef500cd4ed3b2433b8c09dd59a5f9a4
 
 export const Projects = () => {
 
@@ -23,11 +17,7 @@ export const Projects = () => {
             <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-purple-400 bg-clip-text text-transparent text-center"> Featured Projects </h2>
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-<<<<<<< HEAD
                 <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-purple-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-=======
-                <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
->>>>>>> 16f34e0a4ef500cd4ed3b2433b8c09dd59a5f9a4
                     <img src={notBullSht} alt="" className='rounded mb-4'/>
                     <h3 className="text-xl font-bold mb-2"> NotBullSht - Second Hand Store </h3>
                     <p className="text-gray-400 mb-4"> I used my school homework as a start to make this ecommerce </p>
@@ -40,19 +30,11 @@ export const Projects = () => {
                         ))}
                     </div>
                     <div className="flex justify-between items-center">
-<<<<<<< HEAD
                         <a onClick={() => { openSite("https://notbullsht.netlify.app/") }} className="text-blue-400 hover:text-blue-300 transition-colors my-4 cursor-pointer"> View Project → </a>
                     </div>
                 </div>
 
                 <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-purple-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-=======
-                        <a onClick={() => { openSite("https://notbullsht.netlify.app/") }} className="text-blue-400 hover:text-blue-300 transition-colors my-4"> View Project → </a>
-                    </div>
-                </div>
-
-                <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
->>>>>>> 16f34e0a4ef500cd4ed3b2433b8c09dd59a5f9a4
                     <img src={papaleg} alt="" className='rounded mb-4'/>
                     <h3 className="text-xl font-bold mb-2"> Papaléguas Agropet - Petshop </h3>
                     <p className="text-gray-400 mb-4"> I made this website to a Petshop </p>
@@ -65,7 +47,6 @@ export const Projects = () => {
                         ))}
                     </div>
                     <div className="flex justify-between items-center">
-<<<<<<< HEAD
                         <a onClick={() => { openSite("https://www.papaleguasagropet.com/") }} className="text-blue-400 hover:text-blue-300 transition-colors my-4 cursor-pointer"> View Project → </a>
                     </div>
                 </div>
@@ -103,12 +84,6 @@ export const Projects = () => {
                         <a onClick={() => { openSite("https://devlinks-ui.netlify.app/") }} className="text-blue-400 hover:text-blue-300 transition-colors my-4 cursor-pointer"> View Project → </a>
                     </div>
                 </div>
-
-=======
-                        <a onClick={() => { openSite("https://www.papaleguasagropet.com/") }} className="text-blue-400 hover:text-blue-300 transition-colors my-4"> View Project → </a>
-                    </div>
-                </div>
->>>>>>> 16f34e0a4ef500cd4ed3b2433b8c09dd59a5f9a4
             </div>
             
         </div>
